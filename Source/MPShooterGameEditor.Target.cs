813a6b9f36eba7abf20e7b0c7683b647e53b60ff
Brenden <rice.brenden6@gmail.com>
2025-03-17 21:40:01 -0500

    Changed the project to 5.5.4 since I'm silly 🦤🦤

--- Source/MPShooterGameEditor.Target.cs
+++ Source/MPShooterGameEditor.Target.cs
@@ -8,8 +8,8 @@ public class MPShooterGameEditorTarget : TargetRules
 	public MPShooterGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("MPShooterGame");
 	}
 }
